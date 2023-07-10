69486c29276765d0263c8b548770a01cfad7eafc
Subhash Kumar <133902735+subhashkr86@users.noreply.github.com>
2023-07-10 21:44:03 +0530

    Add files via upload

--- Main.js
+++ Main.js
@@ -22,20 +22,19 @@ function Main({ activeNote, onUpdateNote }) {
           onChange={(e) => onEditField("title", e.target.value)}
           autoFocus
         />
-
+        
+        <div className="app-main-note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <ReactMarkdown className="markdown-preview">
+          {activeNote.body}
+        </ReactMarkdown>
+      </div>
         <textarea
           id="body"
           placeholder="Write your code here..."
           value={activeNote.body}
           onChange={(e) => onEditField("body", e.target.value)}
         />
-      </div>
-
-      <div className="app-main-note-preview">
-        <h1 className="preview-title">{activeNote.title}</h1>
-        <ReactMarkdown className="markdown-preview">
-          {activeNote.body}
-        </ReactMarkdown>
       </div>
     </div>
   );
